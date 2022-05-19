fc27ff1daecf65890173ee98399a253b85a3c12c
JMaycol-Valencia <maycolvalencia50@gmail.com>
2022-05-19 18:47:02 -0400

    aplicando_queryMethods_ProductoCrudRepository_ProdoctoRepository

--- src/main/java/com/market/local/persistence/ProductoRepository.java
+++ src/main/java/com/market/local/persistence/ProductoRepository.java
@@ -4,12 +4,21 @@ import com.market.local.persistence.crud.ProductoCrudRepository;
 import com.market.local.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     
-    public List<Producto> getAll{;
+    public List<Producto> getAll(){;
         Iterable<Producto> all = productoCrudRepository.findAll();
         return all;
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return  productoCrudRepository.findByCantidadStockLessThanAndEstadp(cantidad, true);
     }
 }
--- src/main/java/com/market/local/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/market/local/persistence/crud/ProductoCrudRepository.java
@@ -4,5 +4,14 @@ package com.market.local.persistence.crud;
 import com.market.local.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    //creando nuestro primer querymethod
+
+    List<Producto> findByIdCategoriaOrderByAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstadp(int cantidadStock, boolean estado);
+
 }
